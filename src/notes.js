fee8bf2e8ab7ee5aaf487203757af299b435eab7
endard <endardan23@gmail.com>
2024-12-09 17:28:56 +0700

    initial commit

---
+++ src/notes.js
@@ -0,0 +1,14 @@
+const notes = [
+    {
+        id:"asdwq3",
+        tags:["1",2,"sad"],
+        body:"sad3q24",
+    },
+    {
+        id:"2ddq3",
+        tags:["ghsa",5,"sasdad"],
+        body:"sdsg3q24",
+    }
+];
+
+module.exports = notes;
